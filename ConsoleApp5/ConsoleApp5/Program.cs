28d61e8f496896dcd38cb8051b27b848e12c7b5a
PulluriChandana <167088077+PulluriChandana@users.noreply.github.com>
2024-04-23 10:07:40 +0530

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿//using ConsoleApp5;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.ComponentModel;
+using System.Collections.Immutable;
+class Program
+{
+    public static void Main()
+    {
+        Queue queue = new Queue();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+        queue.Enqueue(6);
+        Console.WriteLine("The number of elements present in queue: {0}", queue.Count);
+        while (queue.Count > 0)
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+        Console.WriteLine("The number of elements present in queue: {0}", queue.Count);
+    }
+}
+
