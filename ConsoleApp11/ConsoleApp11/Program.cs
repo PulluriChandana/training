30e60ca7ed6ee502b35d6e0ecf7a2965fd5ce8bb
PulluriChandana <167088077+PulluriChandana@users.noreply.github.com>
2024-05-10 10:25:34 +0530

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Runtime.Intrinsics.Arm;
+using System.Data;
+using System.Xml;
+
+
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;" +
+                                      "Integrated Security=true";
+        int productID = 1;
+        double newunitprice = 22.22;
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand("UpdateProductPrice", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ProductID", productID);
+                command.Parameters.AddWithValue("@NewUnitPrice", newunitprice);
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    Console.WriteLine("Product price updated successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("No data found for specified product ID.");
+                }
+                connection.Close();
+            }
+        }
+    }
+}*/
+
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;" +
+                                      "Integrated Security=true";
+        int employeeId = 5;
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand("GetOrdersByEmployeeAndDateRange", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@EmployeeID", employeeId);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    { 
+                        int employeeid = (int)reader["EmployeeID"];
+                        int startdate = (int)reader["Start Date"];
+                        int enddate = (int)reader["End Date"];
+                        Console.WriteLine($"Employee ID: {employeeId}, Start Date: {startdate},End Date:{enddate}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found for specified customer");
+                }
+                reader.Close();
+                connection.Close();
+            }
+        }
+    }
+}*/
+/*class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;" +
+                                      "Integrated Security=true";
+        string customerId = "ALFKI";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand command = new SqlCommand("CustOrderHist", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@CustomerID", customerId);
+                connection.Open();
+                SqlDataReader reader= command.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string productName = reader["ProductName"].ToString();
+                        int totalOrders = (int)reader["Total"];
+                        Console.WriteLine($"Product Name: {productName}, TotalOrders: {totalOrders}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found for specified customer");
+                }
+                reader.Close();
+                connection.Close();
+            }
+        }
+    }
+}*/
+/*namespace ConsoleApp11
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;"+
+                                      "Integrated Security=true";
+            string queryString = "SELECT * FROM dbo.customers WHERE CustomerID != @customerid";
+
+            string paramValue = "ALFKI";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@customerid",paramValue);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    Console.WriteLine("{0,-20} {1,-20} {2,-30}", 
+                        "Contact Name", "City", "Company Name");
+                    Console.WriteLine("{0,-20} {1,-20} {2,-30}",
+                        "--------------", "------------", "-------------");
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0,-20} {1,-20} {2,-30}", 
+                            reader["ContactName"], reader["City"], reader["CompanyName"]);
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}*/
+
+
+/*namespace ConsoleApp11
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionstring = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;"
+                + "Integrated Security=true";
+            string queryString = "Select EmployeeID,LastName,FirstName from dbo.employees " +
+                    "Where FirstName!=LastName " +
+                    "Order by EmployeeID";
+
+            int paramValue = 5;
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@EmployeeID", paramValue);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}*/
+
+
+/*namespace ConsoleApp11
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionstring = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;" 
+                + "Integrated Security=true";
+            string queryString = "Select ProductID,UnitPrice,ProductName from dbo.products " +
+                    "Where UnitPrice > @PricePoint " +
+                    "Order by UnitPrice DESC";
+
+            int paramValue = 5;
+            using (SqlConnection connection = new SqlConnection(connectionstring))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@PricePoint", paramValue);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                    }
+                    reader.Close();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}*/
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=49C2C7424B625C6;Initial Catalog=amazondb;" +
+                                          "Integrated Security=true";
+            //read data from the customers table from amazondb database
+            DataTable customersDataTable = ReadDataFromamazondb(connectionString, "Select * from Customers");
+            Console.WriteLine(customersDataTable);
+            //display original data
+            Console.WriteLine("Original Customers Data:");
+            DisplayDataTable(customersDataTable);
+            //filter the table where country name is france
+            Console.WriteLine("French country");
+            DataTable frenchCustomersDataTable = FilterData(customersDataTable, "Country='France'");
+            DisplayDataTable(frenchCustomersDataTable);
+            //sort data by company name
+            DataTable sortedCustomersDataTable = SortedData(frenchCustomersDataTable, "CompanyName ASC");
+            DisplayDataTable(sortedCustomersDataTable);
+            //calculate the display the total number of customers
+            int totalCustomers = GetTotalCustomers(customersDataTable);
+            Console.WriteLine($"Total number of customers:{totalCustomers}");
+        }
+        static DataTable ReadDataFromamazondb(string connectionString, string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(data);
+                    }
+                }
+            }
+            return data;
+        }
+        static void DisplayDataTable(DataTable data)
+        {
+            foreach (DataRow row in data.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write($"{item}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static DataTable FilterData(DataTable data, string filterExpression)
+        {
+            DataTable filteredDataTable = data.Clone();
+            DataRow[] filteredRows = data.Select(filterExpression);
+            foreach (DataRow row in filteredRows)
+            {
+                filteredDataTable.ImportRow(row);
+            }
+            return filteredDataTable;
+        }
+
+        static DataTable SortedData(DataTable data, string sortExpression)
+        {
+            DataView dataView = new DataView(data);
+            dataView.Sort = sortExpression;
+            return dataView.ToTable();
+        }
+        static int GetTotalCustomers(DataTable data)
+        {
+            return data.Rows.Count;
+        }
+    }
+}
+
